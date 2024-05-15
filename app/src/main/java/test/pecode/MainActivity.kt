53cb7fce061a5de4e3c5536d9d33628f70ac76e4
roma koval <Romkka636@gmail.com>
2024-05-15 15:05:27 +0300

    Initial commit

---
+++ app/src/main/java/test/pecode/MainActivity.kt
@@ -0,0 +1,17 @@
+package test.pecode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import test.pecode.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
